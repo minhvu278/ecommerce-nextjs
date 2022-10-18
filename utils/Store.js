b4aba5256c80b94f21b16d5b21da5a395f4f7230
minhvu278 <dominhvu278@gmail.com>
2022-10-18 13:23:57 +0700

    create place order screen

--- utils/Store.js
+++ utils/Store.js
@@ -37,6 +37,9 @@ function reducer(state, action) {
                     paymentMethod: ''
                 }
             }
+        case 'CART_CLEAR_ITEMS':
+            return {...state, cart: {...state.cart, cartItems: []}}
+
         case 'SAVE_CART_SHIPPING':
             return {
                 ...state,
